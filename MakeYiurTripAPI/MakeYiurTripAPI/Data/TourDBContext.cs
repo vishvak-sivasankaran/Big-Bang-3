878f19bc3a7e346a14366e15fc273472b5eda0af
vishvak-sivasankaran <127299911+vishvak-sivasankaran@users.noreply.github.com>
2023-07-31 18:09:41 +0530

    created API file

---
+++ MakeYiurTripAPI/MakeYiurTripAPI/Data/TourDBContext.cs
@@ -0,0 +1,12 @@
+﻿using MakeYourTripAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MakeYourTripAPI.Data
+{
+    public class TourDBContext : DbContext
+    {
+        public TourDBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MakeYiurTripAPI/MakeYiurTripAPI/Program.cs
@@ -0,0 +1,45 @@
+using MakeYourTripAPI.Data;
+using MakeYourTripAPI.Repository.Interface;
+using MakeYourTripAPI.Repository.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUser, UserService>();
+
+
+builder.Services.AddCors(opts =>
+{
+    opts.AddPolicy("CORS", options =>
+    {
+        options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+builder.Services.AddDbContext<TourDBContext>(optionsAction: options => options.UseSqlServer(builder.Configuration.GetConnectionString(name: "SQLConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseCors("CORS");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

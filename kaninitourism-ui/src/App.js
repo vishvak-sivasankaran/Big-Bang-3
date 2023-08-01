25492b6003eed0a2b925617e127ecbc863347230
vishvak-sivasankaran <127299911+vishvak-sivasankaran@users.noreply.github.com>
2023-08-01 14:34:42 +0530

    react file upadate

---
+++ kaninitourism-ui/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './Components/LoginPage/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+     hello all
+     <LoginPage />
+    </div>
+  );
+}
+
+export default App;
